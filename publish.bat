a26acc8ac3e159e6a59e8f22c246905208596309
PngS <info@pngs.gr>
2026-02-24 21:33:11 +0200

    Remove publish.bat

--- publish.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Building GKPanStats single-file...
-dotnet publish GKPanStats\GKPanStats.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o publish
-echo.
-echo Done! GKPanStats.exe is in the "publish" folder.
-pause
